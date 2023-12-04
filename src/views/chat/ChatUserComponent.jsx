13ddece7910b7870dc5ea6e6a32eb84b83f38c98
Danel Lafuente <dlafuente003@gmail.com>
2023-12-04 23:50:12 +0100

    hide pokemons in lobby

--- src/views/chat/ChatUserComponent.jsx
+++ src/views/chat/ChatUserComponent.jsx
@@ -1,21 +1,30 @@
 import Pokemon from "../../components/PokemonComponent";
-
+import { useState } from "react";
 
 const ChatUserComponent = ({ user, onClick }) => {
+    const [showPokemons, setShowPokemons] = useState(false);
+    const toggleShowPokemons = () => {
+        setShowPokemons(showPokemons => !showPokemons);
+    }
+
     return (
-        <section className="user-card" key={user.username} onClick={onClick}>
-                    <h2>{user.username}</h2>
-                    <section className="pokemons-container">
-                        {user.pokemons.map((pokemon) => (
-                            <Pokemon
-                                key={pokemon._id}
-                                data={pokemon}
-                                showJustLevel={true}
-                                isCombat={false}
-                            />
-                        ))}
-                    </section>
+        <section className="user-card" key={user.username} onClick={toggleShowPokemons}>
+            <h2>{user.username}</h2>
+            <img className="pokeball-button" src="/swords.svg" alt="attack" onClick={onClick} />
+            {showPokemons &&
+                <section className="pokemons-container">
+                    {user.pokemons.map((pokemon) => (
+                        <Pokemon
+                            key={pokemon._id}
+                            data={pokemon}
+                            showJustLevel={true}
+                            isCombat={false}
+                        />
+                    ))}
+
                 </section>
+            }
+        </section>
     );
 }
 
