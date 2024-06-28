0c39e0260beefe195368155cab5813cd44d8e3af
Danel Lafuente <dlafuente003@gmail.com>
2024-06-28 17:24:32 +0200

    pokemon carousel and css

---
+++ src/components/pokemon/carousel/PokemonCarousel.jsx
@@ -0,0 +1,27 @@
+import Pokemon from "../PokemonComponent"
+import "../Pokemon.scss"
+const PokemonCarousel = ({pokemons,className="pokemon-container",showHp=true,selected=null,onSelect,children=()=>{},disabled=false,fullInfo=false,isCombat=false }) =>{
+    const handleIsPokemonDisabled = (pokemon) => {
+        return disabled || ( pokemon.hp === 0)
+    }
+    return (
+        <section className={className}>
+        {pokemons.map((pokemon) => (
+            <Pokemon
+                key={pokemon._id}
+                data={pokemon}
+                onClick={() => onSelect(pokemon)}
+                isCombat={isCombat}
+                defaultClassName={handleIsPokemonDisabled(pokemon) ? "disabled" : ""}
+                isSelected={selected !== null && selected._id === pokemon._id}
+                fullInfo={fullInfo}
+                showHp={showHp}
+            >
+                {children(pokemon)}
+            </Pokemon>
+        ))}
+    </section>
+    )
+}
+
+export default PokemonCarousel
