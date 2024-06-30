19bc0c4a4892976d8e8bc5b9cbe14c6181376b57
Danel Lafuente <dlafuente003@gmail.com>
2024-06-30 17:05:59 +0200

    title background and gamestates import fixed

--- src/utils/gameStates.js
+++ src/utils/gameStates.js
@@ -10,7 +10,7 @@ import TrainerCombat from "../views/TrainerCombat";
 import GymsView from "../views/GymsView";
 import AdminView from "../views/admin/Admin";
 import Pokedex from "../components/pokedex/Pokedex";
-import Zones from "../components/zone/ZonesAdmin";
+import Zones from "../components/zone/Zones";
 
 const gameStates = {
     "choose": {
